42c301238f6c37e40bffe802dc3ac663b51ba022
cortan-SR25 <cortan.nebojsa@gmail.com>
2024-04-19 19:11:38 +0200

    Initialized port and repository in main

--- backend/main.go
+++ backend/main.go
@@ -1,5 +1,31 @@
 package main
 
+import (
+	"context"
+	"employment-service/repositories"
+	"log"
+	"os"
+	"time"
+)
+
 func main() {
 
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		port = "8000"
+	}
+
+	timeoutContext, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	logger := log.New(os.Stdout, "[product-api] ", log.LstdFlags)
+	storeLogger := log.New(os.Stdout, "[profile-store] ", log.LstdFlags)
+
+	store, err := repositories.New(timeoutContext, storeLogger)
+	if err != nil {
+		logger.Fatal(err)
+	}
+	defer store.Disconnect(timeoutContext)
+
+	store.Ping()
 }
